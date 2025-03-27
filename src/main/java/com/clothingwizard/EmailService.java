c44b58b953385dc50ae7ffe4367ad73685830964
FinalYearProjectWW <c21467734@mytudublin.ie>
2025-03-27 10:53:19 +0000

    Progress on selling feature

---
+++ src/main/java/com/clothingwizard/EmailService.java
@@ -0,0 +1,33 @@
+package com.clothingwizard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import jakarta.mail.internet.MimeMessage;
+
+
+@Service
+public class EmailService {
+	
+	@Autowired
+	private JavaMailSender jms;
+	
+	public void sendInvoiceToRetailer(String email, byte[] invoiceBytes) {
+		try {
+            MimeMessage message = jms.createMimeMessage();
+            MimeMessageHelper mmh = new MimeMessageHelper(message, true);
+            mmh.setTo(email);
+            mmh.setSubject("Your invoice for the Clothing Delivery");
+            mmh.setText("Please find your invoice attached.", true);
+            
+            ByteArrayResource resource = new ByteArrayResource(invoiceBytes);
+            mmh.addAttachment("invoice.pdf", resource);
+            
+            jms.send(message);     
+		}catch(Exception e) {
+			throw new RuntimeException("Error sending email", e);
+		}
+	}
+}
--- src/main/java/com/clothingwizard/InvoiceService.java
+++ src/main/java/com/clothingwizard/InvoiceService.java
@@ -1,8 +1,11 @@
 package com.clothingwizard;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.springframework.stereotype.Service;
 
 
@@ -14,8 +17,41 @@ public class InvoiceService {
 			PDPage page = new PDPage();
 			document.addPage(page);
 			
+			try(PDPageContentStream cs = new PDPageContentStream(document,page)) {
+				cs.beginText();
+				cs.newLineAtOffset(50, 750);
+				cs.showText("Invoice");
+				cs.endText();
+				
+				cs.beginText();
+				cs.newLineAtOffset(50, 700);
+				cs.showText("Retailer: " + pr.getName());
+				cs.endText();
+				
+				cs.beginText();
+				cs.newLineAtOffset(50, 680);
+				cs.showText("Product: " + pr.getProduct());
+				cs.endText();
+				
+				cs.beginText();
+				cs.newLineAtOffset(50, 660);
+				cs.showText("Price Per Unit: " + order.getPrice());
+				cs.endText();
+				
+				cs.beginText();
+				cs.newLineAtOffset(50, 640);
+				cs.showText("Quantity (in Units): " + order.getQuantity());
+				cs.endText();
+				
+				cs.beginText();
+				cs.newLineAtOffset(50, 620);
+				cs.showText("Total Price: " + (order.getQuantity() * order.getPrice()));
+				cs.endText();
+			}
+			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			document.save(baos);
+			return baos.toByteArray();
 		}
-		
 	}
-
 }
